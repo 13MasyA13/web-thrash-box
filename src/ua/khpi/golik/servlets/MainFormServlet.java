30f872a3fa68e8f66ff9977486bb98e09bcaae55
Max Golik <maximko1312@gmail.com>
2018-01-02 14:10:20 +0200

    Get/Post commit
    I have learnt how to get some paramaters from get/post methods

---
+++ src/ua/khpi/golik/servlets/MainFormServlet.java
@@ -0,0 +1,29 @@
+package ua.khpi.golik.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/MainFormServlet")
+public class MainFormServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String userName = request.getParameter("userName");
+		out.println("Hello From GET method " + userName);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String userName = request.getParameter("userName");
+		out.println("Hello From POST method " + userName);
+	}
+
+}
